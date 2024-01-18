f7c91d8c27b8966a76ce666d0dd98e84fa91bf57
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-18 01:09:40 +0100

    commit ok

--- app/src/main/java/com/example/myapplication/PrimerEjercicioActivity.kt
+++ app/src/main/java/com/example/myapplication/PrimerEjercicioActivity.kt
@@ -1,7 +1,13 @@
 package com.example.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.example.myapplication.databinding.ActivityPrimerEjercicioBinding
 
 class PrimerEjercicioActivity : AppCompatActivity() {
@@ -16,6 +22,41 @@ class PrimerEjercicioActivity : AppCompatActivity() {
 
         binding.btnGenerarNotificacion.setOnClickListener {
 
+            createNotificationChannel()
+
+            val intent = Intent(this, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_IMMUTABLE)
+
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_adb)
+                .setContentTitle("Notificación del ejercicio 1")
+                .setStyle(NotificationCompat.BigTextStyle().bigText("Nos invaden los alienígenas!"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .addAction(R.drawable.ic_launcher_foreground, "Ir a la actividad principal", pendingIntent)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(NOTIFICATION_ID, builder.build())
         }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        val name = "TestChannel"
+        val descriptionText = "TestDescription"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance)
+        channel.description = descriptionText
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = 101
+        const val CHANNEL_ID = "channelID"
     }
 }
