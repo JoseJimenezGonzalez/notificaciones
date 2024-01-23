fd0f3fd2ce582d8edff4deba3cba703e78d6faaa
JoseJimenezGonzalez <josexisco1993@gmail.com>
2024-01-23 02:49:32 +0100

    commit falta el ejercicio 3

--- app/src/main/java/com/example/myapplication/SegundoEjercicioActivity.kt
+++ app/src/main/java/com/example/myapplication/SegundoEjercicioActivity.kt
@@ -5,41 +5,78 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.RemoteViews
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import com.example.myapplication.databinding.ActivityPrimerEjercicioBinding
+import com.example.myapplication.databinding.ActivitySegundoEjercicioBinding
 
 class SegundoEjercicioActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityPrimerEjercicioBinding
+    private lateinit var binding: ActivitySegundoEjercicioBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityPrimerEjercicioBinding.inflate(layoutInflater)
+        binding = ActivitySegundoEjercicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnGenerarNotificacion.setOnClickListener {
+        createNotificationChannel()
 
-            createNotificationChannel()
+        binding.btnBigPictureStyle.setOnClickListener {
+
             val idGenerada = GenerarId.crearIdNotificacion()
 
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java).apply{
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK //Para evitar bugs en la aplicacion
+            }
             val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-            // En tu código para construir la notificación
-            val remoteViews = RemoteViews(packageName, R.layout.custom_notification_layout)
-            remoteViews.setImageViewResource(R.id.notification_image, R.drawable.foto_celulas) // Reemplaza con la imagen que deseas mostrar
+            val bigPictureBitmap = BitmapFactory.decodeResource(resources, R.drawable.img_sol)//imagen mas grande
+
 
             val builder = NotificationCompat.Builder(this, GenerarId.CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_adb)
-                .setContentTitle("Título de la notificación")
-                .setContentText("Texto de la notificación")
-                .setCustomContentView(remoteViews) // Configura el RemoteViews personalizado
-                .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSmallIcon(android.R.drawable.ic_menu_gallery)//Icono pequeño
+                .setContentTitle("Captura de pantalla realizada")//Titulo de la notificacion
+                .setContentText("Pulsa para ver la captura de pantalla")//texto
+                //Se muestra o bigTextStyle o bigPictureStyle, no se pueden mostrar ambos de forma concurrente
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bigPictureBitmap))
+                .setContentIntent(pendingIntent)
+                //Como mucho se le pueden meter 3 botones
+                .addAction(android.R.drawable.btn_radio, "Compartir", pendingIntent)
+                .addAction(android.R.drawable.btn_radio, "Borrar", pendingIntent)
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(idGenerada, builder.build())
+        }
+
+
+        binding.btnBigTextStyle.setOnClickListener {
+
+            val idGenerada = GenerarId.crearIdNotificacion()
+
+            val largeIconBitmap = BitmapFactory.decodeResource(resources, R.drawable.img_sol)
+
+            val intent = Intent(this, MainActivity::class.java).apply{
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK //Para evitar bugs en la aplicacion
+            }
+            val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+
+            val builder = NotificationCompat.Builder(this, GenerarId.CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_menu_gallery)//Icono pequeño
+                .setContentTitle("Captura de pantalla realizada")//Titulo de la notificacion
+                .setLargeIcon(largeIconBitmap) // Establecer la imagen como icono grande(aparece a la derecha)
+                .setContentText("Pulsa para ver la captura de pantalla")//texto
+                //Se muestra o bigTextStyle o bigPictureStyle, no se pueden mostrar ambos de forma concurrente
+                .setStyle(NotificationCompat.BigTextStyle().bigText("Esto es una pedazo de descripcion del mensaje. Este es un mensaje de los grandes."))
+                .setContentIntent(pendingIntent)
+                //Como mucho se le pueden meter 3 botones
+                .addAction(android.R.drawable.btn_radio, "Compartir", pendingIntent)
+                .addAction(android.R.drawable.btn_radio, "Borrar", pendingIntent)
 
             val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.notify(idGenerada, builder.build())
@@ -47,17 +84,15 @@ class SegundoEjercicioActivity : AppCompatActivity() {
     }
 
     private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
         val name = "TestChannel"
         val descriptionText = "TestDescription"
         val importance = NotificationManager.IMPORTANCE_DEFAULT
-        val channel = NotificationChannel(GenerarId.CHANNEL_ID, name, importance)
-        channel.description = descriptionText
+        val channel = NotificationChannel(GenerarId.CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
 
         // Register the channel with the system
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
     }
 }
